a719a9d375c4aedd4448452edef54e701151d598
patrickatcw <pj8392@yahoo.com>
2018-02-27 13:54:16 -0500

    created method changeVelocity

--- src/com/me/Car.java
+++ src/com/me/Car.java
@@ -21,9 +21,15 @@ public class Car extends Vehicle{
     }
 
     //create a method for changeGear
-    public void changeGear(int currentGear) {
+    public void changeGear(int currentGear) {   //parameter of currentGear
         this.currentGear = currentGear;     //this is for right here
         System.out.println("Car.setCurrentGear(): Changed to " + this.currentGear + " gear.");
+    }
+
+    //method for changeVelocity
+    public void changeVelocity(int speed, int direction) {  //2 parameters speed, direction
+        System.out.println("Car.changeVelocity(): Velocity " + speed + " direction " + direction);
+        move(speed, direction);   //calling the move method in vehicle class
     }
 
 }
