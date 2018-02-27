838035c6bfe9baae231d40d17e2d20d3590507a9
patrickatcw <pj8392@yahoo.com>
2018-02-27 13:47:05 -0500

    extended class and added to constructor to account for fields from class and extending vehicle

--- src/com/me/Car.java
+++ src/com/me/Car.java
@@ -1,6 +1,6 @@
 package com.me;
 
-public class Car {
+public class Car extends Vehicle{
 
     //add fields before extending class above and creating constructor
     private int wheels;
@@ -11,7 +11,8 @@ public class Car {
     private int currentGear;
 
     //added constructor
-    public Car(int wheels, int doors, int gears, boolean isManual, int currentGear) {
+    public Car(String name, String size, int wheels, int doors, int gears, boolean isManual, int currentGear) {
+        super(name, size);      //needed to add b/c of super and extends purposes
         this.wheels = wheels;
         this.doors = doors;
         this.gears = gears;
