6045106dff02c88bdf7cc7116d0a5c33e175071b
patrickatcw <pj8392@yahoo.com>
2018-02-27 13:30:22 -0500

    created fields and constructor

--- src/com/me/Vehicle.java
+++ src/com/me/Vehicle.java
@@ -1,4 +1,24 @@
 package com.me;
 
 public class Vehicle {
+    //creating fields
+    private String name;    //name of vehicle
+    private String size;    //size of vehicle
+
+    private int currentVelocity;    //speed of vehicle
+    private int currentDirection;   //direction of vehicle
+
+    //create a constructor
+
+
+    public Vehicle(String name, String size /*int currentVelocity, int currentDirection*/) {
+        this.name = name;
+        this.size = size;
+
+        /*this.currentVelocity = currentVelocity;
+        this.currentDirection = currentDirection;*/
+
+        this.currentVelocity = 0;   //removed from parameters and added values of 0
+        this.currentDirection = 0;
+    }
 }
