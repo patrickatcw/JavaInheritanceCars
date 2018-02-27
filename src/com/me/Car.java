1dff812d4c1a012eeff3ae67fcc159722ec65dee
patrickatcw <pj8392@yahoo.com>
2018-02-27 13:41:29 -0500

    added fields

--- src/com/me/Car.java
+++ src/com/me/Car.java
@@ -2,6 +2,14 @@ package com.me;
 
 public class Car {
 
+    //add fields before extending class above and creating constructor
+    private int wheels;
+    private int doors;
+    private int gears;
+    private boolean isManual;
+
+    private int currentGear;
+
 
 
 }
