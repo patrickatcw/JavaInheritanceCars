fa2dbb32666a37d965040bd8351894d06575b37d
patrickatcw <pj8392@yahoo.com>
2018-02-27 14:16:20 -0500

    added field for warranty and made new constructor to include warranty field

--- src/com/me/Ford.java
+++ src/com/me/Ford.java
@@ -2,7 +2,17 @@ package com.me;
 
 public class Ford extends Car {
 
-    public Ford(String name, String size, int wheels, int doors, int gears, boolean isManual, int currentGear) {
+    //added this field specific for car, then remade constructor, which included new field
+    //in addition to what i had created before
+    private int warrantyLength;
+
+    public Ford(String name, String size, int wheels, int doors, int gears, boolean isManual, int currentGear, int warrantyLength) {
         super(name, size, wheels, doors, gears, isManual, currentGear);
+        this.warrantyLength = warrantyLength;
     }
+
+   /* public Ford(String name, String size, int wheels, int doors, int gears, boolean isManual, int currentGear) {
+        super(name, size, wheels, doors, gears, isManual, currentGear);
+    }*/
+
 }
